1f8ed76feeac537164f8a9a7a9319140b117f728
Reuben Ellis <ethriel3695@gmail.com>
2016-08-17 08:33:47 -0600

    Hello World App Week 1 Added buttons and onclick events

--- HelloWorldApp/app/src/main/java/com/elllistech/helloworldapp/MainActivity.java
+++ HelloWorldApp/app/src/main/java/com/elllistech/helloworldapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.elllistech.helloworldapp;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonName1 = (Button)findViewById(R.id.btnName1);
+        Button buttonName2 = (Button)findViewById(R.id.btnName2);
+        final TextView textHello = (TextView)findViewById(R.id.textViewHelloWorld);
+
+        buttonName1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textHello.setTextColor(Color.parseColor("#ffffff"));
+                textHello.setTextSize(22);
+            }
+        });
+
+        buttonName2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textHello.setTextColor(Color.parseColor("#000000"));
+                textHello.setTextSize(14);
+            }
+        });
     }
 }
