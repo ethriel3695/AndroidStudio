19190c2c53adb047bf34662fa8881a449c569087
Reuben Ellis <ethriel3695@gmail.com>
2016-08-27 23:35:52 -0600

    Added new activity

--- SimpleShoppingCart/app/src/main/java/com/elllistech/simpleshoppingcart/MainActivity.java
+++ SimpleShoppingCart/app/src/main/java/com/elllistech/simpleshoppingcart/MainActivity.java
@@ -1,5 +1,7 @@
 package com.elllistech.simpleshoppingcart;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,18 +9,56 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton
-        pcButton = (ImageButton) findViewById(R.id.pcServicesButton);
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageButton
+                pcButton = (ImageButton) findViewById(R.id.pcServicesButton);
+        ImageButton
+                phoneButton = (ImageButton) findViewById(R.id.phoneAppButton);
+        ImageButton
+                webButton = (ImageButton) findViewById(R.id.webAppButton);
+        ImageButton
+                basicButton = (ImageButton) findViewById(R.id.basicPageButton);
+        ImageButton
+                serverButton = (ImageButton) findViewById(R.id.serverDBButton);
+
+        final Context context = this;
+
         pcButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                setContentView(R.layout.service_description);
+                startActivity(new Intent(context, ServicesDescriptionActivity.class));
+            }
+        });
+
+        phoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(context, ServicesDescriptionActivity.class));
+            }
+        });
+
+        webButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(context, ServicesDescriptionActivity.class));
+            }
+        });
+
+        basicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(context, ServicesDescriptionActivity.class));
+            }
+        });
+
+        serverButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(context, ServicesDescriptionActivity.class));
             }
         });
     }
---
+++ SimpleShoppingCart/app/src/main/java/com/elllistech/simpleshoppingcart/ServicesDescriptionActivity.java
@@ -0,0 +1,13 @@
+package com.elllistech.simpleshoppingcart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServicesDescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.services_description);
+    }
+}
