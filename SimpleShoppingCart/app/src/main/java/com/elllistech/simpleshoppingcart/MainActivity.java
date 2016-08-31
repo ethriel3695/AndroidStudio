ea71ea253a6bd7b75f2c31f4763201146f82794a
Reuben Ellis <ethriel3695@gmail.com>
2016-08-30 23:49:58 -0600

    Finalized shopping cart app

--- SimpleShoppingCart/app/src/main/java/com/elllistech/simpleshoppingcart/MainActivity.java
+++ SimpleShoppingCart/app/src/main/java/com/elllistech/simpleshoppingcart/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton
+        final ImageButton
                 pcButton = (ImageButton) findViewById(R.id.pcServicesButton);
         ImageButton
                 phoneButton = (ImageButton) findViewById(R.id.phoneAppButton);
@@ -24,41 +25,83 @@ public class MainActivity extends AppCompatActivity {
                 basicButton = (ImageButton) findViewById(R.id.basicPageButton);
         ImageButton
                 serverButton = (ImageButton) findViewById(R.id.serverDBButton);
+        final String
+                pcServiceTitle = getResources().getString(R.string.pc_page_title),
+                pcServiceText = getResources().getString(R.string.pc_text_description),
+                pcServiceValue = getResources().getString(R.string.pc_service_price),
+                phoneAppTitle = getResources().getString(R.string.phoneApp_page_title),
+                phoneAppText = getResources().getString(R.string.phoneApp_text_description),
+                phoneAppValue = getResources().getString(R.string.phoneApp_service_price),
+                webAppTitle = getResources().getString(R.string.webApp_page_title),
+                webAppText = getResources().getString(R.string.webApp_text_description),
+                webAppValue = getResources().getString(R.string.webApp_service_price),
+                webPageTitle = getResources().getString(R.string.basicWebSite_page_title),
+                webPageText = getResources().getString(R.string.basicPage_text_description),
+                webPageValue = getResources().getString(R.string.basicPage_service_price),
+                serverDBTitle = getResources().getString(R.string.serverDB_page_title),
+                serverDBText = getResources().getString(R.string.serverDB_text_description),
+                serverDBValue = getResources().getString(R.string.serverDB_service_price);
+
 
         final Context context = this;
 
         pcButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(context, ServicesDescriptionActivity.class));
+                Intent descriptionIntent =
+                        new Intent(context, ServicesDescriptionActivity.class);
+                descriptionIntent.putExtra("titles", pcServiceTitle);
+                descriptionIntent.putExtra("services", pcServiceText);
+                descriptionIntent.putExtra("totalCost", pcServiceValue);
+                startActivity(descriptionIntent);
             }
         });
 
         phoneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(context, ServicesDescriptionActivity.class));
+                Intent descriptionIntent =
+                        new Intent(context, ServicesDescriptionActivity.class);
+                descriptionIntent.putExtra("titles", phoneAppTitle);
+                descriptionIntent.putExtra("services", phoneAppText);
+                descriptionIntent.putExtra("totalCost", phoneAppValue);
+                startActivity(descriptionIntent);
             }
         });
 
         webButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(context, ServicesDescriptionActivity.class));
+                Intent descriptionIntent =
+                        new Intent(context, ServicesDescriptionActivity.class);
+                descriptionIntent.putExtra("titles", webAppTitle);
+                descriptionIntent.putExtra("services", webAppText);
+                descriptionIntent.putExtra("totalCost", webAppValue);
+                startActivity(descriptionIntent);
             }
         });
 
         basicButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(context, ServicesDescriptionActivity.class));
+                Intent descriptionIntent =
+                        new Intent(context, ServicesDescriptionActivity.class);
+                descriptionIntent.putExtra("titles", webPageTitle);
+                descriptionIntent.putExtra("services", webPageText);
+                descriptionIntent.putExtra("totalCost", webPageValue);
+                startActivity(descriptionIntent);
             }
         });
 
         serverButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(context, ServicesDescriptionActivity.class));
+                Intent descriptionIntent =
+                        new Intent(context, ServicesDescriptionActivity.class);
+                descriptionIntent.putExtra("titles", serverDBTitle);
+                descriptionIntent.putExtra("services", serverDBText);
+                descriptionIntent.putExtra("totalCost", serverDBValue);
+                startActivity(descriptionIntent);
             }
         });
     }
--- SimpleShoppingCart/app/src/main/java/com/elllistech/simpleshoppingcart/ServicesDescriptionActivity.java
+++ SimpleShoppingCart/app/src/main/java/com/elllistech/simpleshoppingcart/ServicesDescriptionActivity.java
@@ -1,7 +1,12 @@
 package com.elllistech.simpleshoppingcart;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ServicesDescriptionActivity extends AppCompatActivity {
 
@@ -9,5 +14,49 @@ public class ServicesDescriptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.services_description);
+
+        Intent servicesIntent = getIntent();
+
+        String
+                title = servicesIntent.getExtras().getString("titles"),
+                service = servicesIntent.getExtras().getString("services"),
+                totalCost = servicesIntent.getExtras().getString("totalCost");
+
+        TextView
+            serviceTitle = (TextView)findViewById(R.id.descriptionPageTitle),
+            serviceDescription = (TextView)findViewById(R.id.txtServiceFeatures),
+            servicePrice = (TextView)findViewById(R.id.txtPriceValue);
+
+        serviceTitle.setText(title);
+        serviceDescription.setText(service);
+        servicePrice.setText(totalCost);
+
+        Button
+                phoneButton = (Button)findViewById(R.id.backToHome),
+                addToCartButton = (Button)findViewById(R.id.addToCartButton);
+
+        final Context context = this;
+
+        phoneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homePageIntent =
+                        new Intent(context, MainActivity.class);
+                startActivity(homePageIntent);
+            }
+        });
+
+        final TextView
+                shoppingCartBadgeText = (TextView)findViewById(R.id.shoppingCartBadge);
+        addToCartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String badgeText = shoppingCartBadgeText.getText().toString();
+                int shoppingCartCounter = Integer.parseInt(badgeText);
+                shoppingCartCounter++;
+                shoppingCartBadgeText.setText(String.valueOf(shoppingCartCounter));
+            }
+        });
+
     }
 }
